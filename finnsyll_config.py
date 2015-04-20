e16e12adf2eb8e95deb0a3a3533bf69fafaa184f
Naomi T. S <tsnaomi@uw.edu>
2015-04-20 06:04:32 -0700

    update config

--- finnsyll_config.py
+++ finnsyll_config.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import timedelta
 
-local_database = 'postgresql:///aamulehti'
+local_database = 'postgresql:///finnsyll'
 
 PERMANENT_SESSION_LIFETIME = timedelta(days=4)
 SECRET_KEY = os.environ.get('SECRET_KEY') or '31415926535'
