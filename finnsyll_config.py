18b81fabf58b9729f57ba9a5f3941046f9246b08
Naomi T. S <tsnaomi@uw.edu>
2015-05-03 10:21:02 -0700

    tidy up config

--- finnsyll_config.py
+++ finnsyll_config.py
@@ -2,9 +2,7 @@ import os
 
 from datetime import timedelta
 
-local_database = 'postgresql:///finnsyll'
-
 PERMANENT_SESSION_LIFETIME = timedelta(days=4)
-SECRET_KEY = os.environ.get('SECRET_KEY') or '31415926535'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', local_database)
-TESTING = not bool(os.environ.get('DATABASE_URL', 0))
+SECRET_KEY = os.getenv('SECRET_KEY', '31415926535')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///finnsyll')
+TESTING = not bool(os.environ.get('DATABASE_URL'))
