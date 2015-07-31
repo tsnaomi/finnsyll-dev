fca3f1499bba90063e61d49055ab26954cc62a2d
Naomi T. S <tsnaomi@uw.edu>
2015-07-31 13:09:56 -0700

    update config

--- finnsyll_config.py
+++ finnsyll_config.py
@@ -6,9 +6,3 @@ PERMANENT_SESSION_LIFETIME = timedelta(days=4)
 SECRET_KEY = os.getenv('SECRET_KEY', '31415926535')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///finnsyll')
 TESTING = not bool(os.environ.get('DATABASE_URL'))
-
-# if TESTING:
-#     SQLALCHEMY_DATABASE_URI = 'postgresql:///FINNSYLL_BACKUP'
-
-# else:
-#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
