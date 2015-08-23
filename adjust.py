0375ec53198d5ae6814ff1ebbf1a8af19b467590
Naomi T. S <tsnaomi@uw.edu>
2015-08-23 16:09:26 -0700

    revert adjust.py

--- adjust.py
+++ adjust.py
@@ -1,9 +1,7 @@
 # coding=utf-8
 
 from datetime import datetime
-from finnsyll import db, Sequence, Token
-
-from sqlalchemy import or_
+from finnsyll import db, Token
 
 # ADJUST() allows you to apply changes to all of the tokens in the database
 # without completely rendering your computer incapable of doing anything else.
@@ -39,23 +37,5 @@ def ADJUST():
     print 'Adjustment complete. ' + datetime.utcnow().strftime('%I:%M')
 
 
-def ADJUST_SEQUENCES():
-    '''Adjust sequences.'''
-    sequences = Sequence.query.filter(or_(
-        Sequence.sequence.contains('ä'),
-        Sequence.sequence.contains('ö'),
-        ))
-
-    for seq in sequences:
-        vv = seq.sequence
-        word = seq.v_sequence.t_variation.orth.lower()
-        i = seq.html.find('<')
-        j = i + 2
-        seq.html = '%s<strong>%s</strong>%s' % (word[:i], vv, word[j:])
-
-    db.session.commit()
-
-
 if __name__ == '__main__':
-    # ADJUST()
-    ADJUST_SEQUENCES()
+    ADJUST()
