0580424eff5b553a6ef64bfff755ec367e48b120
Naomi T. S <tsnaomi@uw.edu>
2015-09-04 17:10:59 -0700

    update session expiration

--- finnsyll_config.py
+++ finnsyll_config.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import timedelta
 
-PERMANENT_SESSION_LIFETIME = timedelta(days=4)
+PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
 SECRET_KEY = os.getenv('SECRET_KEY', '31415926535')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///finnsyll')
 TESTING = not bool(os.environ.get('DATABASE_URL'))
