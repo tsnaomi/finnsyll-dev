114c9f3396095bc6c25cf1251f988e8d15212597
Naomi T. S <tsnaomi@uw.edu>
2016-01-12 15:39:15 -0800

    commit a commit that comes a little too late

--- syllabifier/compound.py
+++ syllabifier/compound.py
@@ -1,68 +1,73 @@
 # coding=utf-8
 
+# A LOT OF COUNTING AND DIVIDING.
+# CHECKOUT: KenLM, Google Ngram Viewer
+
 import math
 import morfessor
 import re
 
-# from datetime import datetime as dt
 from itertools import izip_longest as izip, product
-from os import sys, path  # , getcwd
+from os import sys, path
 from phonology import (
-    is_cluster,
-    is_consonant,
-    is_coronal,
-    is_harmonic,
-    is_vowel,
+    check_nuclei as nuclei,
+    check_sonseq as sonseq,
+    check_word_final as word_final,
+    is_harmonic as harmonic,
     replace_umlauts,
     )
-# from pprint import pprint
 
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))  # UH OH
+
+import finnsyll as finn
+
+TRAINING = finn.training_set()
+VALIDATION = finn.dev_set()
 
 
-# Morfessor -------------------------------------------------------------------
+# Morfessor: "In morphological segmentation, compounds are word forms,
+# constructions are morphs, and atoms are characters. In chunking, compounds
+# are sentences, constructions are phrases, and atoms are words."
 
-# In morphological segmentation, compounds are word forms, constructions are
-# morphs, and atoms are characters. In chunking, compounds are sentences,
-# constructions are phrases, and atoms are words.
+class DilettanteSplitter(object):
 
-class StupidBackoffMorfessor(object):
+    def __init__(self, training=TRAINING, validation=VALIDATION,
+                 filename='data/morfessor-training', test=False,
+                 maximize='P', Eval=True, train_coefficients=True,
+                 a=1.0, b=0.0, c=0.0, d=0.0, e=0.0, f=0.0):
+        self.training_tokens = training
+        self.validation_tokens = validation
 
-    def __init__(self, filename='data/morfessor-training', test=False,
-                 maximize='P', Eval=True, train_coefficients=True):
-        import finnsyll as finn
-
-        self.training_tokens = finn.training_set()
-        self.validation_tokens = finn.test_set() if test else finn.dev_set()
-
-        # Morfessor model
+        # Morfessor model, or some other morphological analyzer
         self.model = None
 
-        # filename of Training text and Morfessor model binary file
-        prefix = 'syllabifier/' if __name__ != '__main__' else ''
+        # filename of training text and morfessor model binary file
+        prefix = 'syllabifier/' if __name__ != '__main__' else ''  # UH OH
         self.filename = prefix + filename
 
         # ngram containers
-        self.unigrams = {'UNK': 0, 'X': 1, '#': 1}
-        self.bigrams = {'UNK': 0, }
-        self.trigrams = {'UNK': 0, }
+        self.unigrams = {}  # UNK?
+        self.bigrams = {}   # UNK?
+        self.trigrams = {}  # UNK?
         self.total = 0
 
         # evaluation measure to maximize
         self.maximize = {'P': 0, 'R': 1, 'F1': 2, 'F05': 3}[maximize]
 
         # coefficients
-        self.a = 1.0  # ngram
-        self.b = 0.0  # nuclei
-        self.d = 0.0  # coronal
-        self.g = 0.0  # harmonic
-        self.p = 0.0  # sonseq
+        self.a = a  # ngram
+        self.b = b  # nuclei
+        self.c = d  # coronal
+        self.d = d  # harmonic
+        self.e = e  # sonseq
+        self.f = f  # breaks
 
         # evaluation report
         self.report = None
 
         # train segmenter
-        self.train(train_coefficients=train_coefficients)
+        self.train_coefficients = False if a < 1.0 else train_coefficients
+        self.train()
 
         # evaluate segmenter
         if Eval:
@@ -72,7 +77,7 @@ class StupidBackoffMorfessor(object):
         self._train_morfessor()
         self._train_ngrams()
 
-        if train_coefficients:
+        if self.train_coefficients:
             self._train_coefficients()
 
     def _train_morfessor(self):
@@ -110,6 +115,53 @@ class StupidBackoffMorfessor(object):
 
     def _train_ngrams(self):
         print 'Training ngrams...'
+
+        for t in self.training_tokens:
+            stems = re.split(r'=|-| ', t.gold_base)
+            morphemes = map(
+                lambda m: m.replace(' ', '').replace('-', ''),
+                self.model.viterbi_segment(t.orth.lower())[0],
+                )
+
+            # create word of form [#, morpheme1, X, morpheme2, #]
+            word = []
+            index = -1
+            indices = [-1, ]
+
+            for stem in stems:
+                index += len(stem)
+                indices.append(index)
+
+            index = -1
+
+            for morpheme in morphemes:
+                word.append('#' if index in indices else 'X')
+                word.append(morpheme)
+                index += len(morpheme)
+
+            word.append('#')
+
+            # get unigram, bigram, and trigram counts
+            for i, morpheme in enumerate(word):
+                self.unigrams.setdefault(morpheme, 0)
+                self.unigrams[morpheme] += 1
+                self.total += 1
+
+                if i > 0:
+                    bigram = word[i-1] + morpheme
+                    self.bigrams.setdefault(bigram, 0)
+                    self.bigrams[bigram] += 1
+
+                if i > 1:
+                    trigram = word[i-2] + bigram
+                    self.trigrams.setdefault(trigram, 0)
+                    self.trigrams[trigram] += 1
+
+    def _train_ngrams_old(self):  # out of commission
+        print 'Training ngrams...'
+
+        self.unigrams['X'] = 1
+        self.unigrams['#'] = 1
 
         for t in self.training_tokens:
             stems = re.split(r'=|-| ', t.gold_base)
@@ -175,12 +227,12 @@ class StupidBackoffMorfessor(object):
         try:
             with open(filename, 'r') as f:
                 coefficients = [float(i) for i in f.read().split()]
-                self.a, self.b, self.d, self.g, self.p = coefficients
+                self.a, self.b, self.d, self.g, self.p, self.k = coefficients
 
         except IOError:
             # generate all possible sets of coefficients
             coefficients = [float(i) / 100 for i in range(0, 101, 5)]
-            coefficients = product(coefficients, repeat=5)
+            coefficients = product(coefficients, repeat=6)
             coefficients = filter(lambda x: sum(x) == 1.0, coefficients)
 
             # further filter coefficients so that ngram scores cannot receive a
@@ -200,7 +252,7 @@ class StupidBackoffMorfessor(object):
                 sys.stdout.write(message % (i, count))
                 sys.stdout.flush()
 
-                self.a, self.b, self.d, self.g, self.p = co
+                self.a, self.b, self.d, self.g, self.p, self.k = co
                 score = self.evaluate(Eval=False)[self.maximize]
                 scored_coefficients.append((score, co))
 
@@ -209,7 +261,7 @@ class StupidBackoffMorfessor(object):
 
             # set coefficients that maximizes the f1 score of the training data
             coefficients = max(scored_coefficients)[1]
-            self.a, self.b, self.d, self.g, self.p = coefficients
+            self.a, self.b, self.d, self.g, self.p, self.k = coefficients
 
             print (
                 '\na=%s\tngram\nb=%s\tnuclei\nd=%s\tcoronal\ng=%s\tharmonic'
@@ -263,12 +315,12 @@ class StupidBackoffMorfessor(object):
         bad = len(results['bad'])  # 27
 
         # calculate precision, recall, and F1
-        # P = ((TP + bad) * 1.0) / ((TP + bad) + FP)
-        # R = ((TP + bad) * 1.0) / ((TP + bad) + FN)
-        P = (TP * 1.0) / (TP + FP + bad)  # 0.6503
+        P = (TP * 1.0) / (TP + FP + bad)  # 0.6514
         R = (TP * 1.0) / (TP + FN)        # 0.9867
-        F1 = (2.0 * P * R) / (P + R)      # 0.7840
-        F05 = ((0.5**2 + 1.0) * P * R) / ((0.5**2 * P) + R)  # 0.6979
+        F1 = (2.0 * P * R) / (P + R)      # 0.7848
+        F05 = ((0.5**2 + 1.0) * P * R) / ((0.5**2 * P) + R)  # 0.6989
+
+        # TODO: perplexity measure
 
         if not Eval:
             return P, R, F1, F05
@@ -276,12 +328,12 @@ class StupidBackoffMorfessor(object):
         # generate evaluation report
         self.report = (
             '\n'
-            '---- Evaluation: StupidBackoffMorfessor -------------------------'
+            '---- Evaluation: DilettanteSplitter -----------------------------'
             # '\n\nTrue negatives:\n\t%s'
             # '\n\nTrue positives:\n\t%s'
-            # '\n\nFalse negatives:\n\t%s'
-            # '\n\nFalse positives:\n\t%s'
-            # '\n\nBad segmentations:\n\t%s'
+            '\n\nFalse negatives:\n\t%s'
+            '\n\nFalse positives:\n\t%s'
+            '\n\nBad segmentations:\n\t%s'
             '\n\nMaximizing: %s (0=P, 1=R, 2=F1, 3=F0.5)'
             '\n\nTP:\t%s\nFP:\t%s\nTN:\t%s\nFN:\t%s\nBad:\t%s'
             '\n\nP/R:\t%s / %s\nF1:\t%s\nF0.5:\t%s\n\n'
@@ -290,10 +342,10 @@ class StupidBackoffMorfessor(object):
             ) % (
                 # '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['TN']]),
                 # '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['TP']]),
-                # '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['FN']]),
-                # '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['FP']]),
-                # '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['bad']]),
-                'N/A' if self.a == 1.0 else self.maximize,
+                '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['FN']]),
+                '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['FP']]),
+                '\n\t'.join(['%s (%s)' % (w, t) for w, t in results['bad']]),
+                'N/A' if not self.train_coefficients else self.maximize,
                 TP,
                 FP,
                 TN,
@@ -383,39 +435,21 @@ class StupidBackoffMorfessor(object):
 
             C_count = self.unigrams.get(C, 0) + 1
             score += math.log(C_count * 0.4)
-            score -= math.log(self.total + len(self.unigrams))
-
-        return score, candidate
-
-    def _score_phonotactics(self, score, candidate):
-
-        def _nuclei(segment):
-            return len(filter(is_vowel, segment)) > 1
-
-        def _coronal(segment):
-            return is_vowel(segment[-1]) or is_coronal(segment[-1])
-
-        def _harmonic(segment):
-            return is_harmonic(segment)
-
-        def _sonseq(segment):
-            onset = re.split(r'(i|e|A|y|O|a|u|o)', segment)[0]
-
-            if onset and is_consonant(onset[0]):
-                return is_consonant(onset) or is_cluster(onset)
-
-            return True
+            score -= math.log(self.total + len(self.unigrams))  # UNK?
 
         # convert score from negative to positive
         score = 100.0 - (score * -1.0)
         score /= 100.0
 
-        # convert candidate from list to string to normalize it
+        # convert candidate from list to string
         del candidate[0]
         del candidate[-1]
         candidate = ''.join(candidate).replace('#', '=').replace('X', '')
 
-        if self.a < 1.0:
+        return score, candidate
+
+    def _score_phonotactics(self, score, candidate):
+        if self.a < 1:
 
             # get suggested segmentations as list
             segments = replace_umlauts(candidate).split('=')
@@ -423,21 +457,21 @@ class StupidBackoffMorfessor(object):
             if len(segments) > 1:
 
                 # score phonotactic features
-                nuclei = 1 if all(_nuclei(seg) for seg in segments) else 0
-                coronal = 1 if all(_coronal(seg) for seg in segments) else 0
-                harmonic = 1 if all(_harmonic(seg) for seg in segments) else 0
-                sonseq = 1 if all(_sonseq(seg) for seg in segments) else 0
+                nuclei = 1 if all(nuclei(seg) for seg in segments) else 0
+                coronal = 1 if all(word_final(seg) for seg in segments) else 0
+                harmonic = 1 if all(harmonic(seg) for seg in segments) else 0
+                sonseq = 1 if all(sonseq(seg) for seg in segments) else 0
+                breaks = 1.0 / len(segments)
 
                 score *= self.a
                 score += nuclei * self.b
-                score += coronal * self.d
-                score += harmonic * self.g
-                score += sonseq * self.p
+                score += coronal * self.c
+                score += harmonic * self.d
+                score += sonseq * self.e
+                score += breaks * self.f
 
         return score, candidate
 
-
-# -----------------------------------------------------------------------------
 
 def delimit(word):
     '''Insert syllable breaks at non-delimited compound boundaries.'''
@@ -445,8 +479,9 @@ def delimit(word):
 
 
 if __name__ == '__main__':
-    # StupidBackoffMorfessor(maximize='P')
-    # StupidBackoffMorfessor(maximize='R')
-    # StupidBackoffMorfessor(maximize='F1')
-    StupidBackoffMorfessor(maximize='F05')
-    # StupidBackoffMorfessor(train_coefficients=True)
+    # DilettanteSplitter(maximize='P')
+    # DilettanteSplitter(maximize='R')
+    # DilettanteSplitter(maximize='F1')
+    # DilettanteSplitter(maximize='F05')
+    # DilettanteSplitter(train_coefficients=False)
+    DilettanteSplitter(a=0.8, b=0.1, c=0.0, d=0.0, e=0.0, f=0.1)
