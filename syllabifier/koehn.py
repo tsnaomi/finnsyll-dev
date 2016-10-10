944dd123cfc02b97974b858c054d05fb90673bda
Naomi T. S <tsnaomi@uw.edu>
2016-10-10 10:41:06 -0700

    save linking morpheme option before discarding

--- syllabifier/koehn.py
+++ syllabifier/koehn.py
@@ -12,6 +12,7 @@ import sys
 from collections import defaultdict
 from datetime import datetime as dt
 from nltk.stem.snowball import FinnishStemmer
+from itertools import product
 
 from phonology import replace_umlauts
 
@@ -24,41 +25,72 @@ FS = FinnishStemmer()
 
 class Koehn:
 
-    def __init__(self, validation=None, evaluate=True):
+    def __init__(self, validation=None, pos_filtering=True, transform=False):
         self.validation_tokens = validation
-        self.filename = './data/koehn/corpus.pickle'
+
+        if pos_filtering:
+            self.filename = './data/koehn/corpus-filtered-no_proper.pickle'
+
+        else:
+            self.filename = './data/koehn/corpus.pickle'
 
         # gather corpus frequencies
         print 'Gathering corpus frequencies... ' + dt.utcnow().strftime('%I:%M')  # noqa
-        self.corpus = self.get_corpus()
+        self.corpus = self.get_corpus(pos_filtering)
+
+        if transform:
+            # get words ending in "-nen"
+            print 'Gathering nens... ' + dt.utcnow().strftime('%I:%M')
+            self.nens = self.get_nens(pos_filtering)
+
+            self.get_candidates = self._get_stripped_candidates
+            self.score_candidate = self._score_stripped_candidate
+
+        else:
+            self.get_candidates = self._get_candidates
+            self.score_candidate = self._score_candidate
 
         # segment and evaluate
-        if evaluate:
-            print 'Segmenting... ' + dt.utcnow().strftime('%I:%M')
-            self.evaluate()
+        print 'Segmenting... ' + dt.utcnow().strftime('%I:%M')
+        self.evaluate()
 
         print 'Complete. ' + dt.utcnow().strftime('%I:%M')
 
-    def get_corpus(self):
+    def get_corpus(self, pos_filtering):
+        # If pos_filtering, exclude the types Conjunction, Interjection,
+        # Numeral, Preposition, and Proper. Include Adjective, Adjective-Noun,
+        # Adverb, CompPart, Noun, Noun-Noun, Pronoun, and Verb.
         try:
             with open(self.filename) as f:
                 corpus = pickle.load(f)
 
         except IOError:
+
+            if pos_filtering:
+                query = finn.Token.query.filter(finn.and_(
+                    finn.Token.pos != 'Conjunction',
+                    finn.Token.pos != 'Interjection',
+                    finn.Token.pos != 'Numeral',
+                    finn.Token.pos != 'Preposition',
+                    finn.Token.pos != 'Proper',
+                    ))
+            else:
+                query = finn.Token.query
+
             corpus = defaultdict(int)
-            count = finn.Token.query.count()
+            count = query.count()
             start = 0
             end = x = 1000
 
             while start + x < count:
 
-                for t in finn.Token.query.order_by(finn.Token.id).slice(start, end):  # noqa
+                for t in query.order_by(finn.Token.id).slice(start, end):
                     corpus[t.orth.lower()] += t.freq
 
                 start = end
                 end += x
 
-            for t in finn.Token.query.order_by(finn.Token.id).slice(start, count):  # noqa
+            for t in query.order_by(finn.Token.id).slice(start, count):
                 corpus[t.orth.lower()] += t.freq
 
             with open(self.filename, 'wb') as f:
@@ -66,17 +98,67 @@ class Koehn:
 
         return corpus
 
+    def get_nens(self, pos_filtering):
+        filename = self.filename.replace('corpus', 'nens')
+
+        try:
+            with open(filename) as f:
+                nens = pickle.load(f)
+
+        except IOError:
+
+            if pos_filtering:
+                query = finn.Token.query.filter(finn.and_(
+                    finn.Token.pos != 'Conjunction',
+                    finn.Token.pos != 'Interjection',
+                    finn.Token.pos != 'Numeral',
+                    finn.Token.pos != 'Preposition',
+                    finn.Token.pos != 'Proper',
+                    ))
+            else:
+                query = finn.Token.query
+
+            # nens = defaultdict(int)
+            nens = []
+            count = query.count()
+            start = 0
+            end = x = 1000
+
+            while start + x < count:
+
+                for t in query.order_by(finn.Token.id).slice(start, end):
+                    word = t.orth.lower()
+
+                    if word.endswith('nen'):
+                        nens.append(word)
+
+                start = end
+                end += x
+
+            for t in query.order_by(finn.Token.id).slice(start, count):
+                word = t.orth.lower()
+
+                if word.endswith('nen'):
+                    nens.append(word)
+
+            with open(filename, 'wb') as f:
+                pickle.dump(nens, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        return nens
+
     def segment(self, word):
         word = word.lower()  # normalize
-        candidates = self._get_candidates(word)
-        scored_candidates = [self._score_candidate(c) for c in candidates]
-        best = max(scored_candidates)  # [1]
+        candidates = self.get_candidates(word)
 
-        scores = [sc for sc in scored_candidates if sc[0] == best[0]]
-        if len(scores) > 1:
-            print scores, best[1]
+        try:
+            scored_candidates = [self.score_candidate(k, v) for k, v in candidates.items()]  # noqa
 
-        return best[1]
+        except AttributeError:
+            scored_candidates = [self.score_candidate(c) for c in candidates]
+
+        best = max(scored_candidates)[1]
+
+        return best
 
     def _get_candidates(self, word):
 
@@ -93,7 +175,30 @@ class Koehn:
             return result
 
         # get all splits of minimum length 3
-        return split(word)
+        candidates = split(word)
+
+        return candidates
+
+    def _get_stripped_candidates(self, word):
+
+        def swap(seg):
+            return seg[:-1] + 'nen'
+
+        def derivable(seg):
+            return seg.endswith('s') and swap(seg) in self.nens
+
+        candidates = self._get_candidates(word)
+        candidate_dict = {c: [] for c in candidates}
+
+        for cand in list(candidates):
+            # 2 ^ n - 1 new candidates
+            parts = [[s, swap(s)] if derivable(s) else [s, ] for s in cand.split('=')]  # noqa
+            new = ['='.join(constituents) for constituents in product(*parts)]
+            candidate_dict[cand].extend(new)
+
+        candidate_dict = {k: list(set(v)) for k, v in candidate_dict.items()}
+
+        return candidate_dict
 
     def _score_candidate(self, candidate):
         constituents = candidate.split('=')
@@ -111,7 +216,13 @@ class Koehn:
 
         score *= (1.0 / len(constituents))
 
-        return (score, candidate)
+        return score, candidate
+
+    def _score_stripped_candidate(self, candidate, options):
+        scored_options = [self._score_candidate(c) for c in options]
+        score = max(scored_options)[0]
+
+        return score, candidate
 
     def evaluate(self):
         # results include true positives, false positives, true negatives,
@@ -161,12 +272,12 @@ class Koehn:
             '\n\nFalse negatives:\n\t%s'
             '\n\nFalse positives:\n\t%s'
             '\n\nBad segmentations:\n\t%s'
-            '\n\n'
+            '\n'
             '\n\tTP:\t%s\n\tFP:\t%s\n\tTN:\t%s\n\tFN:\t%s\n\tBad:\t%s'
             '\n\tP/R:\t%s / %s\n\tF1:\t%s\n\tF0.5:\t%s\n\tAcc.:\t%s'
             '\n\n'
             '-----------------------------------------------------------------'
-            '\n\n'
+            '\n'
             ) % (
                 '\n\t'.join(['%s (%s)' % t for t in results['FN']]),
                 '\n\t'.join(['%s (%s)' % t for t in results['FP']]),
@@ -193,4 +304,4 @@ class Koehn:
 if __name__ == '__main__':
     # VALIDATION = finn.dev_set()
     VALIDATION = finn.test_set()
-    Koehn(validation=VALIDATION)
+    Koehn(validation=VALIDATION, pos_filtering=True)
