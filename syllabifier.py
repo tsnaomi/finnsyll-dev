688315c626959387d1959d5a56878b839fb296cf
Naomi T. S <tsnaomi@uw.edu>
2016-10-22 12:30:48 -0700

    simplify syllabifier.py

--- syllabifier.py
+++ syllabifier.py
@@ -2,14 +2,11 @@
 
 from os import sys, path
 
-try:
-    # import local finnsyll project with unreleased developments
-    projects = path.dirname(path.dirname(path.abspath(__file__)))
-    sys.path = [path.join(projects, 'finnsyll'), ] + sys.path
-    from finnsyll import FinnSyll
+# import local finnsyll project with unreleased developments;
+# otherwise, import pip installed finnsyll package
+projects = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path = [path.join(projects, 'finnsyll'), ] + sys.path
 
-except Exception as e:
-    # import pip installed finnsyll package
-    from finnsyll import FinnSyll
+from finnsyll import FinnSyll
 
 FinnSyll = FinnSyll()
