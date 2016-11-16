43541df649de684665eca52b8d7efecf62ea3b2e
Naomi T. S <tsnaomi@uw.edu>
2016-11-16 14:26:30 -0800

    allow access to local FinnSyll development

--- syllabifier.py
+++ syllabifier.py
@@ -7,19 +7,6 @@ from os import sys, path
 projects = path.dirname(path.dirname(path.abspath(__file__)))
 sys.path = [path.join(projects, 'finnsyll'), ] + sys.path
 
-from finnsyll import FinnSyll
+from finnsyll import FinnSyll, phonology as phon  # noqa
 
 FinnSyll = FinnSyll(track_rules=True)
-
-
-def replace_umlauts(word, put_back=False):  # use translate()
-    '''If put_back is True, put in umlauts; else, take them out!'''
-    if put_back:
-        word = word.replace(u'A', u'ä').replace(u'A', u'\xc3\xa4')
-        word = word.replace(u'O', u'ö').replace(u'O', u'\xc3\xb6')
-
-    else:
-        word = word.replace(u'ä', u'A').replace(u'\xc3\xa4', u'A')
-        word = word.replace(u'ö', u'O').replace(u'\xc3\xb6', u'O')
-
-    return word
