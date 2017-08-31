fab1cfdecad716339ba50e6f61b6a5bc7189839c
Naomi T. S <tsnaomi@uw.edu>
2017-08-31 13:34:11 -0700

    revise config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from datetime import timedelta
 PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
 SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-TESTING = os.getenv('TESTING')
+TESTING = os.getenv('TESTING', False)
 DEBUG = True
