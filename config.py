c465131ec6af088f2851554e085332b039727bde
Naomi T. S <tsnaomi@uw.edu>
2017-08-31 13:05:35 -0700

    update config.py for server use

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 from datetime import timedelta
 
 PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
-SECRET_KEY = os.getenv('SECRET_KEY', '31415926535')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///finnsyll')
-TESTING = not bool(os.environ.get('DATABASE_URL'))
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+TESTING = os.getenv('TESTING')
+DEBUG = True
