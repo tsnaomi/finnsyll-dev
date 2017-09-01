e8dd4424c26bf1c6b21e2ab38ab028228a97d13a
Naomi T. S <tsnaomi@uw.edu>
2017-08-31 22:39:07 -0700

    ignore config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from datetime import timedelta
-
-PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
-SECRET_KEY = os.getenv('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-TESTING = os.getenv('TESTING', False)
-DEBUG = True
