728557e9510e0227649fbddef26a07cae2aa44b7
Naomi Shapiro <tsnaomi@uw.edu>
2017-09-22 16:40:12 -0700

    automate deleting older pg backups

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,2 +1,7 @@
+# delete backups that are more than 10 days old
+find "$(pwd)/pg_backups" -mtime +10 -exec rm {} \;
+
+
+# back up the finnsyll database
 fn="$(pwd)/pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
 pg_dump finnsyll > $fn
