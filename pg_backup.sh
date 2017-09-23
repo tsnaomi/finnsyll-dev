248bc16e0a78ff841411f5192bc37b093950d84e
Naomi Shapiro <tsnaomi@uw.edu>
2017-09-23 09:31:52 -0700

    restructure pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,19 +1,19 @@
 {   # LOCAL ATTEMP
 
-    # delete backups that are more than 10 days old
-    find "$(pwd)/pg_backups" -mtime +10 -exec rm {} \;
-
     # back up the metric-gold database
     fn="$(pwd)/pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
     pg_dump metric-gold > $fn
 
+    # delete backups that are more than 10 days old
+    find "$(pwd)/pg_backups" -mtime +10 -exec rm {} \;
+
 } || {  # SERVER ATTEMPT
+
+    # back up the metric-gold database
+    fn="~/finnsyll-dev/pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
+    pg_dump metric-gold > $fn
 
     # delete backups that are more than 10 days old
     find "~/finnsyll-dev/pg_backups" -mtime +10 -exec rm {} \;
-
-    # back up the metric-gold database
-    fn="~/finnsyll-dev//pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
-    pg_dump metric-gold > $fn
 
 }
