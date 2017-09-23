48e6b11b81637fa03b096b0d872873700cf9c81c
Naomi Shapiro <tsnaomi@uw.edu>
2017-09-23 09:40:14 -0700

    fix stupid mistake

--- pg_backup.sh
+++ pg_backup.sh
@@ -2,7 +2,7 @@
 
     # back up the metric-gold database
     fn="$(pwd)/pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
-    pg_dump metric-gold > $fn
+    pg_dump finnsyll > $fn
 
     # delete backups that are more than 10 days old
     find "$(pwd)/pg_backups" -mtime +10 -exec rm {} \;
@@ -11,7 +11,7 @@
 
     # back up the metric-gold database
     fn="~/finnsyll-dev/pg_backups/finnsyll-$(date +%d%b%Y-%H:%M)"
-    pg_dump metric-gold > $fn
+    pg_dump finnsyll > $fn
 
     # delete backups that are more than 10 days old
     find "~/finnsyll-dev/pg_backups" -mtime +10 -exec rm {} \;
